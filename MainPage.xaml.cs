2f42c597058300f688ff6e13fc889ebe0426cbaf
Kristian Pinke <kristian.pinke@outlook.com>
2021-04-09 14:34:27 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ReportItemBase itemBase;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = this;
+
+            itemBase = new ReportItemBase
+            {
+                Title = "Item 1.3",
+                Items = new ObservableCollection<ReportItemBase>(new[]
+                {
+                    new ReportItemBase {Title = "Item 1.3.1"},
+                    new ReportItemBase {Title = "1.3.2"},
+                    new ReportItemBase {Title = "Item 1.3.3"}
+                })
+            };
+
+            DataSource = new ObservableCollection<ReportItemBase>(new[]
+            {
+                new ReportItemBase
+                {
+                    Title = "Item 1",
+                    Items = new ObservableCollection<ReportItemBase>(new[]
+                    {
+                        new ReportItemBase
+                        {
+                            Title = "Item 1.1",
+                            Items = new ObservableCollection<ReportItemBase>(new[]
+                            {
+                                new ReportItemBase
+                                {
+                                    Title = "Item 1.1.1"
+                                }
+                            })
+                        },
+                        new ReportItemBase
+                        {
+                            Title = "Item 1.2"
+                        },
+                        itemBase })
+                    }
+            });
+        }
+
+        private ObservableCollection<ReportItemBase> DataSource;
+
+        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            itemBase.IsOpened = !itemBase.IsOpened;
+        }
+    }
+}
---
+++ ReportItemBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App2
+{
+    [Windows.UI.Xaml.Data.Bindable]
+    public class ReportItemBase : INotifyPropertyChanged
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title == value)
+                    return;
+
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set
+            {
+                if (_isChecked == value)
+                    return;
+
+                _isChecked = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isOpened;
+        public bool IsOpened
+        {
+            get => _isOpened;
+            set
+            {
+                if (_isOpened == value)
+                    return;
+
+                _isOpened = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<ReportItemBase> _items;
+        public ObservableCollection<ReportItemBase> Items
+        {
+            get => _items ?? (_items = new ObservableCollection<ReportItemBase>());
+
+            set => _items = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
